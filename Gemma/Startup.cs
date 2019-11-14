f5e93cabad7508cb78097416b001e0e9c1ec6e4f
John <john804122@gmail.com>
2019-11-14 19:40:34 +0800

    Fisrt commet

---
+++ Gemma/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Gemma.Startup))]
+namespace Gemma
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
