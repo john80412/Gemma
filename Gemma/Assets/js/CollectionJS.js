6826ba5eb71acf8d14130f47d599aed6f043b59e
ChienShan <patrick0533066@gmail.com>
2019-11-21 17:36:06 +0800

    FirstCollection

---
+++ Gemma/Assets/js/CollectionJS.js
@@ -0,0 +1,40 @@
+﻿$(function () {
+
+    //img.hover
+    $('.collection-item').hover(
+
+        function () //滑鼠移過去做的事
+        {
+            $(this).css("cursor", "pointer");
+            $(this).css("transform", "scale(0.8)").css("transition-duration", "0.5s"); //放大與變形時間
+
+        },
+        function () //滑鼠移出做的事
+        {
+            $(this).css("transform", "scale(1)").css("transition-duration", "0.5s");
+        }
+    );
+
+    $('img').hover(
+
+        function () //滑鼠移過去做的事
+        {
+
+            $('img').css("opacity", "0.3");
+            $(this).css("opacity", "1");
+        },
+        function () //滑鼠移出做的事
+        {
+
+            $('img').css("opacity", "1");
+        }
+    );
+
+    $('a').click(
+        function ()
+        {
+            $('img').css("opacity", "1");
+        },
+    );  
+
+});
---
+++ Gemma/Controllers/CollectionController.cs
@@ -0,0 +1,45 @@
+﻿using Gemma.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gemma.Controllers
+{
+    public class CollectionController : Controller
+    {
+        // GET: Collection
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CollectionPhotos()
+        {
+            List<CollectionItem> photos = new List<CollectionItem>
+            {
+                new CollectionItem { Name = "test", FileName = "1.jpg", Price = 16000, Weburl = "#"},
+                new CollectionItem { Name = "test", FileName = "2.jpg", Price = 16000, Weburl = "#"},
+                new CollectionItem { Name = "test", FileName = "3.jpg", Price = 16000, Weburl = "#"},
+                new CollectionItem { Name = "test", FileName = "4.jpg", Price = 16000, Weburl = "#"},
+                new CollectionItem { Name = "test", FileName = "5.jpg", Price = 16000, Weburl = "#"},
+                new CollectionItem { Name = "test", FileName = "6.jpg", Price = 16000, Weburl = "#"},
+                new CollectionItem { Name = "test", FileName = "7.jpg", Price = 16000, Weburl = "#"},
+                new CollectionItem { Name = "test", FileName = "8.jpg", Price = 16000, Weburl = "#"},
+                new CollectionItem { Name = "test", FileName = "9.jpg", Price = 16000, Weburl = "#"},
+                new CollectionItem { Name = "test", FileName = "10.jpg", Price = 16000, Weburl = "#"},
+                new CollectionItem { Name = "test", FileName = "11.jpg", Price = 16000, Weburl = "#"},
+                new CollectionItem { Name = "test", FileName = "12.jpg", Price = 16000, Weburl = "#"},
+                new CollectionItem { Name = "test", FileName = "13.jpg", Price = 16000, Weburl = "#"},
+                new CollectionItem { Name = "test", FileName = "14.jpg", Price = 16000, Weburl = "#"},
+                new CollectionItem { Name = "test", FileName = "15.jpg", Price = 16000, Weburl = "#"},
+                new CollectionItem { Name = "test", FileName = "16.jpg", Price = 16000, Weburl = "#"},
+                new CollectionItem { Name = "test", FileName = "17.jpg", Price = 16000, Weburl = "#"},
+                new CollectionItem { Name = "test", FileName = "18.jpg", Price = 16000, Weburl = "#"},
+            };
+
+            return View(photos);
+        }
+    }
+}
