7dd651000d3ba497da3b1c10934304d8b684c096
Yuan <mlovyuan@yahoo.com.tw>
2019-11-27 20:44:47 +0800

    會員中心_修改會員資料和查訂單

---
+++ Gemma/Models/MetaData/AspNetUserMD.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Gemma.Models
+{
+    [MetadataType(typeof(AspNetUserMD))]
+    public partial class AspNetUser
+    {
+        public class AspNetUserMD
+        {
+            public string Id { get; set; }
+            /// <summary>
+            /// 登錄信息
+            /// </summary>
+            [Display(Name = "Eメール *")]
+            [DataType(DataType.EmailAddress)]
+            [Required(ErrorMessage = "Required")]
+            [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Error type")]
+            public string Email { get; set; }
+
+            [Display(Name = "パスワード *")]
+            [DataType(DataType.Password)]
+            [Required(ErrorMessage = "Required")]
+            [StringLength(20, MinimumLength = 6, ErrorMessage = "Password must be at least 6 digits")]
+            public string Password { get; set; }
+
+            [Display(Name = "パスワード（再入力）*")]
+            [DataType(DataType.Password)]
+            [StringLength(20, MinimumLength = 6, ErrorMessage = "Password must be at least 6 digits")]
+            [Required(ErrorMessage = "Required")]
+            [Compare("Password", ErrorMessage = "Doesn't match the password")]
+            public string CheckPassword { get; set; }
+            /// <summary>
+            /// 注文者住所
+            /// </summary>
+            [Display(Name = "姓 *")]
+            [Required(ErrorMessage = "Required")]
+            public string LastName { get; set; }
+
+            [Display(Name = "名 *")]
+            [Required(ErrorMessage = "Required")]
+            public string FirstName { get; set; }
+
+            [Display(Name = "電話番号 *")]
+            [DataType(DataType.PhoneNumber)]
+            [Required(ErrorMessage = "Required")]
+            public string Mobile { get; set; }
+
+            [Display(Name = "国名")]
+            public string Country { get; set; }
+
+            [Display(Name = "郵便番号")]
+            [DataType(DataType.PostalCode)]
+            public string PostalCode { get; set; }
+
+            [Display(Name = "住所")]
+            public string Address { get; set; }
+
+            [Display(Name = "生年月日 *")]
+            [DataType(DataType.Date), DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+            [Required(ErrorMessage = "Required")]
+            public DateTime? DateOfBirth { get; set; }
+        }  
+    }
+}
---
+++ Gemma/Repository/MemberCenterRepository.cs
@@ -0,0 +1,34 @@
+﻿using Gemma.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using static Gemma.Models.AspNetUser;
+
+namespace Gemma.Repository
+{
+    public class MemberCenterRepository
+    {
+        internal GemmaDBContext db = new GemmaDBContext();
+        /// <summary>
+        /// 找到 User 資料庫的 id 資料，也可以改抓Email
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public AspNetUser GetMemberData(string id)
+        {
+            AspNetUser member = db.AspNetUsers.Find(id);
+            return member;
+        }
+
+        /// <summary>
+        /// 抓 User 的訂單
+        /// </summary>
+        /// <returns></returns>
+        public List<Order> GetOrder(string id)
+        {
+            var order = db.Orders.Where((d) => d.CustomerID == id).ToList();
+            return order;
+        }
+    }
+}
