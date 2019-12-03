76c9bf39f7da95679de537dfb94768e5cda61419
John <john804122@gmail.com>
2019-12-03 15:17:47 +0800

    create product manager

---
+++ Gemma/ViewModel/ProductViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Gemma.ViewModel
+{
+    public class ProductViewModel
+    {
+        [Key]
+        public int ProductID { get; set; }
+        [Display (Name ="產品名稱")]
+        public string ProductName { get; set; }
+        [Display(Name = "單價")]
+        public int UnitPrice { get; set; }
+        [Display(Name = "折扣")]
+        public decimal Discount { get; set; }
+        public int CategoryID { get; set; }
+        [Display(Name = "說明")]
+        public string Explain { get; set; }
+        [Display(Name = "類別名稱")]
+        public string CategoryName { get; set; }
+
+    }
+}
