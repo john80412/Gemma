3bd48cbdc5e86a87617e69757f2f6c1c75206197
John <john804122@gmail.com>
2019-12-05 14:36:13 +0800

    create order details

--- Gemma/Controllers/OrderController.cs
+++ Gemma/Controllers/OrderController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -18,5 +19,18 @@ namespace Gemma.Controllers
             page = search == "true" ? 1 : page;
             return View(rep.GetSearchStock(customerName, productNames,page));
         }
+        //public ActionResult Details(int? orderID)
+        //{
+        //    if (orderID == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    var stock = rep.GetOrderDetail(productID, colorID, sizeID);
+        //    if (stock == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(stock);
+        //}
     }
 }
