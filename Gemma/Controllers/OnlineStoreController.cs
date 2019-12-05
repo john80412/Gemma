801417c559bda533fddd9b9d00579e971b321211
John <john804122@gmail.com>
2019-12-05 18:14:01 +0800

    mark echo

--- Gemma/Controllers/OnlineStoreController.cs
+++ Gemma/Controllers/OnlineStoreController.cs
@@ -19,21 +19,21 @@ namespace Gemma.Controllers
 
         public ActionResult OnlineStore()
         {
-            public List<Stock> GetShoesPicture()
-            {
-                var Shoes = new List<Stock>();
-                var ShoesItem = db.Stocks.ToList();
-                foreach (var item in ShoesItem)
-                {
-                    Shoes.Add(new Stock()
-                    {
-                        Id = item.ProductID,
-                        Picture = item.ImageName,
-                        Color = new string[] { }
-                    });
-                }
-                return Shoes;
-            }
+            //public List<Stock> GetShoesPicture()
+            //{
+            //    var Shoes = new List<Stock>();
+            //    var ShoesItem = db.Stocks.ToList();
+            //    foreach (var item in ShoesItem)
+            //    {
+            //        Shoes.Add(new Stock()
+            //        {
+            //            Id = item.ProductID,
+            //            Picture = item.ImageName,
+            //            Color = new string[] { }
+            //        });
+            //    }
+            //    return Shoes;
+            //}
 
             return View();
         }
