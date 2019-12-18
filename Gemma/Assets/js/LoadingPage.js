7ca94045000b5ab5688d5dde557919707891589e
ming wu <a85062@pchome.com.tw>
2019-12-18 17:57:35 +0800

    DaniellaGemma頁面22~24行加入Loading Page

---
+++ Gemma/Assets/js/LoadingPage.js
@@ -0,0 +1,9 @@
+﻿setTimeout(
+    function () {
+        window.addEventListener("load", function () {
+            var load = document.querySelector(".loading");
+            load.classList.add("hidden");
+        });
+    }
+    , 2500
+);
