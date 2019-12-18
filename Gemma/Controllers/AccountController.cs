f1bd4d4657667ff713b02589bd0191b5bc7c4844
art031565 <art031565@gmail.com>
2019-12-18 14:00:38 +0800

    Change Views ForgotPassword, ResetPassword, ExternalLoginConfirmation, Login, _ExternalLoginsListPartial ... Interface

--- Gemma/Controllers/AccountController.cs
+++ Gemma/Controllers/AccountController.cs
@@ -238,10 +238,10 @@ namespace Gemma.Controllers
 
                 // 如需如何進行帳戶確認及密碼重設的詳細資訊，請前往 https://go.microsoft.com/fwlink/?LinkID=320771
                 // 傳送包含此連結的電子郵件
-                // string code = await UserManager.GeneratePasswordResetTokenAsync(user.Id);
-                // var callbackUrl = Url.Action("ResetPassword", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);		
-                // await UserManager.SendEmailAsync(user.Id, "重設密碼", "請按 <a href=\"" + callbackUrl + "\">這裏</a> 重設密碼");
-                // return RedirectToAction("ForgotPasswordConfirmation", "Account");
+                string code = await UserManager.GeneratePasswordResetTokenAsync(user.Id);
+                var callbackUrl = Url.Action("ResetPassword", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                await UserManager.SendEmailAsync(user.Id, "重設密碼", "請按 <a href=\"" + callbackUrl + "\">這裏</a> 重設密碼");
+                return RedirectToAction("ForgotPasswordConfirmation", "Account");
             }
 
             // 如果執行到這裡，發生某項失敗，則重新顯示表單
@@ -489,7 +489,7 @@ namespace Gemma.Controllers
             {
                 return Redirect(returnUrl);
             }
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Home", "MemberCenter");
         }
 
         internal class ChallengeResult : HttpUnauthorizedResult
