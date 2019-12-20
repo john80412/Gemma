6dd008568c299757880decc11941eaf45707f73d
John <john804122@gmail.com>
2019-12-20 17:51:29 +0800

    speed up onlinestore

--- Gemma/Controllers/OnlineStoreController.cs
+++ Gemma/Controllers/OnlineStoreController.cs
@@ -18,10 +18,32 @@ namespace Gemma.Controllers
         private OnlineStoreRepository repo = new OnlineStoreRepository();
         public ActionResult FindBrand(string CategoryName = "ALL", string ColorName = "ALL", string OrderBy = "None")
         {
+            if (Session["ListProducts"] == null)
+            {
+
+                var StoredProcedureVM = repo.db.Database.SqlQuery<SingleProductViewModel>("exec SingleProductViewModel").AsQueryable();
+                var ListProducts = (from p in StoredProcedureVM
+                                    select new OnlineStoreProductVM
+                                    {
+                                        ProductId = p.ProductId,
+                                        ProductName = p.ProductName,
+                                        UnitPrice = p.UnitPrice,
+                                        CategoryName = p.CategoryName
+                                    }).Distinct(new OnlineStoreProductVMCompare()).ToList();
+                foreach (var item in ListProducts)
+                {
+                    item.ColorName = new List<string>();
+                    foreach (var color in StoredProcedureVM.Where(x => x.ProductId == item.ProductId))
+                    {
+                        item.ColorName.Add(color.ColorName + ".jpg");
+                    }
+                }
+                Session["ListProducts"] = ListProducts;
+            }
             Session["CategoryName"] = CategoryName;
             Session["ColorName"] = ColorName;
             Session["PriceOrderBy"] = OrderBy;
-            var ProductVM = repo.GetProductsSearch(CategoryName, ColorName, OrderBy);
+            var ProductVM = repo.GetProductsSearch(CategoryName, ColorName, OrderBy, ((List<OnlineStoreProductVM>)Session["ListProducts"]).AsQueryable());
             Session["ProductModel"] = ProductVM;
             return View(ProductVM);
         }
