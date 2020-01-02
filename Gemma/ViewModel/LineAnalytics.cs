4434288d77523b25121317fd0869124f18735348
Yuan <mlovyuan@yahoo.com.tw>
2020-01-02 13:58:07 +0800

    upload LinePay

---
+++ Gemma/ViewModel/LineAnalytics.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gemma.ViewModel
+{
+    public class PaymentUrl
+    {
+        public string web { get; set; }
+        public string app { get; set; }
+    }
+
+    public class Info
+    {
+        public long transactionId { get; set; }
+        public PaymentUrl paymentUrl { get; set; }
+        public string paymentAccessToken { get; set; }
+    }
+
+    public class LineRootObject
+    {
+        public string returnCode { get; set; }
+        public string returnMessage { get; set; }
+        public Info info { get; set; }
+    }
+}
